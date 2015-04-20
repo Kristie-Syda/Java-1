d5bf77cf8a1767169dbb199fe764a70db1d2f062
Kristie-Syda <ksyda@fullsail.edu>
2015-04-19 22:58:45 -0500

    Created spinner, adapter and array

--- AdvancedViews/app/src/main/java/com/kristiesyda/advancedviews/MainActivity.java
+++ AdvancedViews/app/src/main/java/com/kristiesyda/advancedviews/MainActivity.java
@@ -4,14 +4,23 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    Spinner spinner;
+    ArrayAdapter arrAdapt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Attach Adapter to spinner
+        spinner = (Spinner) findViewById(R.id.mainSpin);
+
     }
 
 }
