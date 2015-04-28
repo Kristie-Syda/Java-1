bce0168871f8f32272319b8b3d440c65076b66b4
Kristie-Syda <ksyda@fullsail.edu>
2015-04-28 06:05:09 -0500

    start-over

--- Connected_Application/app/src/main/java/com/kristiesyda/connected_application/MainActivity.java
+++ Connected_Application/app/src/main/java/com/kristiesyda/connected_application/MainActivity.java
@@ -6,6 +6,8 @@ import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import java.io.BufferedInputStream;
@@ -25,33 +27,42 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Getting our connectivity manager.
-        ConnectivityManager mgr = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
-        // Getting our active network information.
-        NetworkInfo netInfo = mgr.getActiveNetworkInfo();
-        //checks connection.
-        if(netInfo != null) {
-            CharSequence text;
-            if (netInfo.isConnected()) {
-                // We have a valid data connection
-                text = "Connected";
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    URL url = new URL("");
+                }
             }
-            else {
-                text = "No Connection";
-            }
-            Context context = getApplicationContext();
-            int duration = Toast.LENGTH_SHORT;
-            Toast toast = Toast.makeText(context, text, duration);
-            toast.show();
-        }
+        });
+
+//        // Getting our connectivity manager.
+//        ConnectivityManager mgr = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+//        // Getting our active network information.
+//        NetworkInfo netInfo = mgr.getActiveNetworkInfo();
+//        //checks connection.
+//        if(netInfo != null) {
+//            CharSequence text;
+//            if (netInfo.isConnected()) {
+//                // We have a valid data connection
+//                text = "Connected";
+//            }
+//            else {
+//                text = "No Connection";
+//            }
+//            Context context = getApplicationContext();
+//            int duration = Toast.LENGTH_SHORT;
+//            Toast toast = Toast.makeText(context, text, duration);
+//            toast.show();
+//        }
 
         // The URL string that points to our web resource.
-        //String urlString = "http://www.example.com/some_api";
-        // Creating the URL object that points to our web resource.
+        String urlString = "https://api.themoviedb.org/3/movie/550?api_key=62dbead3af716cd1edf3092f3be3bf5e";
         URL url = null;
         try {
-            url = new URL("https://api.themoviedb.org/3/movie/550?api_key=62dbead3af716cd1edf3092f3be3bf5e");
-
+            url = new URL(urlString);
+            // Establish a connection to the resource at the URL.
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             // Setting connection properties.
             connection.setRequestMethod("GET");
@@ -61,22 +72,18 @@ public class MainActivity extends ActionBarActivity {
             connection.connect();
             // Optionally check the status code. Status 200 means everything went OK.
             int statusCode = connection.getResponseCode();
+            System.out.println(statusCode);
             // Getting the InputStream with the data from our resource.
-            InputStream stream = connection.getInputStream();
+
             // Reading data from the InputStream using the Apache library.
-            String resourceData = IOUtils.toString(stream);
-            // Cleaning up our connection resources.
-            stream.close();
+            String resourceData = IOUtils.toString(connection.getInputStream());
+            System.out.println(resourceData);
+
             connection.disconnect();
             // The resourceData string should now have our data.
-
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
 
 
 
